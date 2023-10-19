4356b2e029e5eaaf74ed16aa97f65f08398aca27
dominikhable <habledominik@gmail.com>
2023-10-19 18:10:56 +0200

    Add project files.

---
+++ AoC/src/Puzzles/2015/Day01/Puzzle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.src.Puzzles._2015.Day01
+{
+    public class Puzzle
+    {
+        private readonly string _input;
+
+        public Puzzle(string input)
+        {
+            _input = input;
+            Console.WriteLine("Result 1: " + SolveTask1());
+            Console.WriteLine("Result 2: " + SolveTask2());
+        }
+
+        public string SolveTask1()
+        {
+            var floor = 0;
+
+            foreach(var c in _input)
+            {
+                floor += c is '(' ? 1 : -1;
+            }
+
+            return floor.ToString();
+        }
+
+        public string SolveTask2()
+        {
+            var floor = 0;
+            var count = 0;
+
+            while(floor is not -1)
+            {
+                floor += _input[count] is '(' ? 1 : -1;
+                count++;
+            }
+
+            return count.ToString();
+        }
+    }
+}
---
+++ AoC/src/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.src.Repository
+{
+    public class Repository
+    {
+        public Type getTaskType(string year, string day)
+        {
+            validateYearAndDay(year, ref day);
+
+            day = addZeroIfNeeded(day);
+            var path = $"AoC.src.Puzzles._{year}.Day{day}.Puzzle";
+            Type? taskType = Type.GetType(path);
+            if (taskType is null)
+            {
+                throw new FileNotFoundException();
+            }
+            return taskType;
+        }
+
+        private void validateYearAndDay(string year, ref string day)
+        {
+            if(!isBetween(Int32.Parse(year), 2015, DateTime.Now.Year))
+            {
+                throw new ArgumentException($"{year} is not a valid year.");
+            }
+            
+            if (!isBetween(Int32.Parse(day), 1, 25))
+            {
+                throw new ArgumentException($"{day} is not a valid day.");
+            }
+
+            day = addZeroIfNeeded(day);
+        }
+
+        public void Solve(Type taskType, string input)
+        {
+            Activator.CreateInstance(taskType, input);
+        }
+
+        private string addZeroIfNeeded(string day) => day.Length is 2 ? day : $"0{day}";
+
+        private bool isBetween(int n, int min, int max) => n >= min && n <= max;
+    }
+}
