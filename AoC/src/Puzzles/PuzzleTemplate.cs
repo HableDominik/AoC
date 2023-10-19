b28c56799bd9f1e6302cb4fba74ed9f2eb51ead0
dominikhable <habledominik@gmail.com>
2023-10-19 19:44:51 +0200

    add solution 2015 day 2

---
+++ AoC/src/Puzzles/PuzzleTemplate.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC.src.Puzzles._20XX.DayXX
+{
+    public class Puzzle
+    {
+        private readonly string _input;
+
+        public Puzzle(string input)
+        {
+            _input = input;
+            Console.WriteLine("Result 1: " + SolveTask1());
+            Console.WriteLine("Result 2: " + SolveTask2());
+        }
+
+        public string SolveTask1()
+        {
+            return string.Empty;
+        }
+
+        public string SolveTask2()
+        {
+            return string.Empty;
+        }
+    }
+}
