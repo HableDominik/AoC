8f4b693808d07f0528fb766b4134fc1fc7bc731a
dominikhable <habledominik@gmail.com>
2023-10-20 16:15:09 +0200

    move readLines into Util

--- AoC/src/Puzzles/2015/Day02/Puzzle.cs
+++ AoC/src/Puzzles/2015/Day02/Puzzle.cs
@@ -7,7 +7,7 @@
         public Puzzle(string input)
         {
             _presents = new List<Present>();
-            var dimensions = readLines(input);
+            var dimensions = Util.readLines(input);
             dimensions.ForEach(dimension => _presents.Add(new Present(dimension)));
 
             Console.WriteLine($"Result 1: {SolveTask1()}");
@@ -17,11 +17,5 @@
         public int SolveTask1() => _presents.Sum(present => present.calculateSurface());
 
         public int SolveTask2() => _presents.Sum(present => present.calculateRibbon());      
-
-        private List<string> readLines(string input) // TODO: utils
-        {
-            return input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
-        }
-
     }
 }
---
+++ AoC/src/Util/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.src
+{
+    public static class Util
+    {
+        public static List<string> readLines(string input) 
+            => input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
+    }
+}
