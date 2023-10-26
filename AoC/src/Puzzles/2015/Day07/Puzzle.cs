fb90e5b251854569ed14f51e8ecc44907315815d
dominikhable <habledominik@gmail.com>
2023-10-26 21:37:56 +0200

    add Solution 2015 Day07

--- AoC/src/Puzzles/2015/Day07/Puzzle.cs
+++ AoC/src/Puzzles/2015/Day07/Puzzle.cs
@@ -1,59 +1,123 @@
-﻿namespace AoC.src.Puzzles._2015.Day07
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AoC.src.Puzzles._2015.Day07
 {
     public class Puzzle
     {
         private readonly string _input;
+        private List<Wire> _wires = new List<Wire>();
 
         delegate ushort Operator(ushort a, ushort b);
 
-        public static ushort And(ushort a, ushort b) => (ushort)(a & b);
-        public static ushort Or(ushort a, ushort b) => (ushort)(a | b);
-        public static ushort LShift(ushort a, ushort b) => (ushort)(a << b);
-        public static ushort RShift(ushort a, ushort b) => (ushort)(a >> b);
-        public static ushort Not(ushort a, ushort b) => (ushort)~a;
+        private static ushort And(ushort a, ushort b) => (ushort)(a & b);
+        private static ushort Or(ushort a, ushort b) => (ushort)(a | b);
+        private static ushort LShift(ushort a, ushort b) => (ushort)(a << b);
+        private static ushort RShift(ushort a, ushort b) => (ushort)(a >> b);
+        private static ushort Not(ushort a, ushort b) => (ushort)~a;
+        private static ushort Is(ushort a, ushort b) => a;
 
+        private record Wire (string Name, string? LHS, string? RHS, Operator? Op, ushort? Value) { }
 
         public Puzzle(string input)
         {
             _input = input;
-            Console.WriteLine($"Result 1: {SolveTask1()}");
-            Console.WriteLine($"Result 2: {SolveTask2()}");
+            GetWires();
+            var result1 = SolveTask1();
+            Console.WriteLine($"Result 1: {result1}");
+            Console.WriteLine($"Result 2: {SolveTask2(result1)}");
         }
 
-        public int SolveTask1()
+        public ushort SolveTask1()
         {
-            testOperations();
-            return 0;
+            List<Wire> wires = new(_wires);
+            while (wires.Any(wire => wire.Value is null))
+            {
+                ResolveWires(ref wires);
+            }
+            return GetWireValue("a", wires) ?? throw new Exception("Result is null!");
         }
 
-        private void testOperations()
+        private static ushort? GetWireValue(string wireName, List<Wire> wires)
         {
-            ushort x = 123;
-            ushort y = 456;
-            Operator and = And;
-            ushort d = and(x, y);
-            Operator or = Or;
-            ushort e = or(x, y);
-            Operator lshift = LShift;
-            ushort f = lshift(x, 2);
-            Operator rshift = RShift;
-            ushort g = rshift(y, 2);
-            Operator not = Not;
-            ushort h = not(x, 0);
-            ushort i = not(y, 0);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(g);
-            Console.WriteLine(h);
-            Console.WriteLine(i);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            if (wireName is "") return 0;
+            return wires.FirstOrDefault(w => w.Name == wireName)?.Value ?? null;
         }
 
-        public int SolveTask2()
+        private void ResolveWires(ref List<Wire> wires)
         {
-            return 0;
+            for (int i = 0; i < wires.Count; i++)
+            {
+                var wire = wires[i];
+                if (wire.Value is not null || wire.LHS is null || wire.RHS is null) continue;
+                var LHS = GetWireValue(wire.LHS, wires);
+                if (LHS is null) continue;
+                var RHS = GetWireValue(wire.RHS, wires);
+                if (RHS is null) continue;
+                wires[i] = wire with { Value = wire.Op((ushort)LHS, (ushort)RHS) };
+            }
+
+        }
+
+        private void GetWires()
+        {
+            foreach(string w in Util.readLines( _input ))
+            {
+                var subs = w.Split(' ');
+                if (subs[1] == "->")
+                {
+                    if (IsNumeric(subs[0]))
+                        _wires.Add(new Wire(subs[2], null, null, null, ushort.Parse(subs[0])));
+                    else
+                        _wires.Add(new Wire(subs[2], subs[0], string.Empty, Is, null));
+
+                }
+                else if (subs[0] == "NOT")
+                {
+                    _wires.Add(new Wire(subs[3], subs[1], string.Empty, Not, null));
+                }
+                else
+                {
+                    _wires.Add(new Wire(subs[4], subs[0], subs[2], GetOp(subs[1]), null));
+                    if (IsNumeric(subs[0]) && !_wires.Any(w => w.Name == subs[0]))
+                    {
+                        _wires.Add(new Wire(subs[0], null, null, null, ushort.Parse(subs[0])));
+                    }
+                    else if (IsNumeric(subs[2]) && !_wires.Any(w => w.Name == subs[2]))
+                    {
+                        _wires.Add(new Wire(subs[2], null, null, null, ushort.Parse(subs[2])));
+                    }
+                }
+            }
+        }
+
+        private static bool IsNumeric(string x) => Regex.IsMatch(x, @"^\d+$");
+
+        private Operator? GetOp(string op) => op switch
+        {
+            "AND" => And,
+            "OR" => Or,
+            "LSHIFT" => LShift,
+            "RSHIFT" => RShift,
+            _ => throw new InvalidDataException(op)
+        };
+
+        public int SolveTask2(ushort result1)
+        {
+            List<Wire> wires = new(_wires);
+            ChangeWireValue(ref wires, "b", result1);          
+            while (wires.Any(wire => wire.Value is null))
+            {
+                ResolveWires(ref wires);
+            }
+            return GetWireValue("a", wires) ?? throw new Exception("Result is null!");
+        }
+
+        private static void ChangeWireValue(ref List<Wire> wires, string wire, ushort value)
+        {
+            int index = wires.FindIndex(w => w.Name == "b");
+            if (index is -1) throw new EntryPointNotFoundException(wire);
+            wires[index] = wires[index] with { Value = value };
         }
     }
 }
