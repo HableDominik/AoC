72ba2059ef8a43695cd8858925875e8a5cb6eea7
dominikhable <habledominik@gmail.com>
2023-10-27 14:54:23 +0200

    add Solution 2015 Day10

---
+++ AoC/src/Puzzles/2015/Day10/Puzzle.cs
@@ -0,0 +1,39 @@
+﻿namespace AoC.src.Puzzles._2015.Day10
+{
+    public class Puzzle
+    {
+        private readonly string _input;
+
+        public Puzzle(string input)
+        {
+            _input = input;
+            //Console.WriteLine($"Result 1: {SolveTask1()}");
+            Console.WriteLine($"Result 2: {SolveTask2()}");
+        }
+
+        public int SolveTask1() => Generate(40, _input).Length;
+
+        private string Generate(int iterations, string input)
+        {
+            Console.WriteLine(iterations);
+            string result = string.Empty;
+            int j = 0;
+            char current = input[0];
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals(current)) j++;
+                else
+                {
+                    result += $"{j}{current}";
+                    j = 1;
+                    current = input[i];
+                }
+            }
+            result += $"{j}{current}";
+            if (iterations > 1) return Generate(--iterations, result);
+            return result;
+        }
+
+        public int SolveTask2() => Generate(50, _input).Length;
+    }
+}
