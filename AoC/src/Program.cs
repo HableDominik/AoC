8fd203c7e7332bb3b1afe86ced67b52928e49fe6
dominikhable <habledominik@gmail.com>
2023-12-17 22:02:24 +0100

    add Solution 2023 Day16

--- AoC/src/Program.cs
+++ AoC/src/Program.cs
@@ -8,7 +8,7 @@ namespace AoC
         public static void Main()
         {
             var year = "2023";
-            var day = "9";
+            var day = "16";
 
             var repository = new Repository();
 
--- AoC/src/Puzzles/2023/Day16/Puzzle.cs
+++ AoC/src/Puzzles/2023/Day16/Puzzle.cs
@@ -6,36 +6,24 @@ namespace AoC.src.Puzzles._2023.Day16
     {
         private readonly int _size;
         private readonly Dictionary<Point, char> _mirrors;
-        private List<Laser> _lasers;
 
         public class Laser
         {
             public Point Pos { get; set; }
             public Point Dir { get; set; }
 
-            public Laser(Point pos, Point dir)
-            {
-                Pos = pos;
-                Dir = dir;
-            }
+            public Laser(Point pos, Point dir) { Pos = pos; Dir = dir; }
 
-            public void Move()
-            {
-                Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y);
-            }
+            public void Move() { Pos = new Point(Pos.X + Dir.X, Pos.Y + Dir.Y); }
 
-            public void ChangeDir(Point dir)
-            {
-                Dir = dir;
-            }
+            public void ChangeDir(Point dir) { Dir = dir; }
         }
 
         public Puzzle(string input)
         {
             var lines = Util.readLines(input);
-            _size = lines.Count();
+            _size = lines.Count;
             _mirrors = ParseMirrors(lines);
-            _lasers = new List<Laser>();
             Console.WriteLine($"Result 1: {SolveTask1()}");
             Console.WriteLine($"Result 2: {SolveTask2()}");
         }
@@ -57,70 +45,74 @@ namespace AoC.src.Puzzles._2023.Day16
             return mirrors;
         }
 
-        public string SolveTask1()
-        {
-            _lasers.Add(new Laser(new(0,0), new(1,0)));
-            for(int i = 0; i < 20; i++)
-            {
-                foreach (var laser in _lasers)
-                {
-                    var mirror = _mirrors.GetValueOrDefault(laser.Pos);
-                    var dir = laser.Dir;
-                    if(mirror != (char)default)
-                        laser.ChangeDir(GetDir(mirror, laser.Pos, dir));
-                    laser.Move();
-                    var pos = laser.Pos;
-                    //if(pos.X < 0 || pos.Y < 0 || pos.X >= _size || pos.Y >= _size)
-                        
-                }
-                Print();
-            }
+        public int SolveTask1() => Energize(new Laser(new(0, 0), new(1, 0)));
 
-            return string.Empty;
+        public int SolveTask2()
+        {
+            var max = 0;
+            for (int i = 0; i < _size; i++)
+            {
+                max = Math.Max(max, Energize(new Laser(new(i, 0), new(0, 1))));
+                max = Math.Max(max, Energize(new Laser(new(0, i), new(1, 0))));
+                max = Math.Max(max, Energize(new Laser(new(i, _size - 1), new(0, -1))));
+                max = Math.Max(max, Energize(new Laser(new(_size - 1, i), new(-1, 0))));
+                Console.WriteLine(i + " " + max);
+            }
+            return max;
         }
 
-        private Point GetDir(char mirror, Point pos, Point dir)
+        private int Energize(Laser start)
+        {
+            var lasers = new List<Laser>() { start };
+            var visited = new HashSet<Point>();
+            var oldVisited = -1;
+            var spazi = 0;
+            while (visited.Count != oldVisited || spazi < 10)
+            {
+                if (spazi > 0 && visited.Count > oldVisited) spazi = 0;
+                if (visited.Count == oldVisited) spazi++;
+                oldVisited = visited.Count;
+
+                var lasersToAdd = new List<Laser>();
+
+                foreach (var laser in lasers)
+                {
+                    var pos = laser.Pos;
+                    if (pos.X < 0 || pos.Y < 0 || pos.X >= _size || pos.Y >= _size)
+                    {
+                        continue;
+                    }
+                    visited.Add(pos);
+
+                    var mirror = _mirrors.GetValueOrDefault(laser.Pos);
+                    laser.ChangeDir(GetDir(mirror, laser.Pos, laser.Dir, lasersToAdd));
+                    laser.Move();
+                }
+                lasers.AddRange(lasersToAdd);
+            }
+            return oldVisited;
+        }
+
+        private static Point GetDir(char mirror, Point pos, Point dir, List<Laser> newLasers)
         {
             switch (mirror)
             {
-                case '/': return new Point(2* dir.X + dir.Y, 2* dir.Y + dir.X);
+                case '/': return new Point(dir.Y * -1, dir.X * -1);
                 case '\\': return new Point(dir.Y, dir.X);
                 case '|':
                     {
                         if (dir.X == 0) return dir;
-                        _lasers.Add(new Laser(pos, new Point(0, -1)));
+                        newLasers.Add(new Laser(pos, new Point(0, -1)));
                         return new Point(0, 1);
                     }
                 case '-':
                     {
                         if (dir.Y == 0) return dir;
-                        _lasers.Add(new Laser(pos, new Point(-1, 0)));
+                        newLasers.Add(new Laser(pos, new Point(-1, 0)));
                         return new Point(1, 0);
                     }
                 default: return dir;
             }
-        }
-
-        private void Print()
-        {
-            for (int row = 0; row < _size; row++)
-            {
-                for (int col = 0; col < _size; col++)
-                {
-                    var current = new Point(col, row);
-                    var symbol = _mirrors.GetValueOrDefault(current);
-                    if (symbol == (char)default)
-                        symbol = _lasers.Any(l => l.Pos == current) ? '#' : (char)default;
-                    Console.Write(symbol == (char)default ? "." : symbol);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-
-        public string SolveTask2()
-        {
-            return string.Empty;
         }
     }
 }
